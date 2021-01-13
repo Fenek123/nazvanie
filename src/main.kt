f709d3b90ede68b12a927ebab52ade6224262fc2
4elove4ek <edlodneva@gmail.com>
2021-01-13 11:17:14 +0700

    Это мой проект!

---
+++ src/main.kt
@@ -0,0 +1,58 @@
+fun main() {
+     print("Вас вітає компанія мтс ми щас вас взловаем. ")
+     print("Введіть першу цифру")
+     var num1 = readLine().toString().toDouble()
+     print("введіть другу цифру")
+     var num2 = readLine().toString().toDouble()
+     var operation = readLine()
+
+//     if(operation == "+")
+//     {
+//          println(num1+num2)
+//     }
+//     if(operation == "-")
+//     {
+//          println(num1-num2)
+//     }
+//     if(operation == "*")
+//     {
+//          println(num1*num2)
+//     }
+//     if(operation == "/")
+//     {
+//          println(num1/num2)
+//     }
+//     else println("ТИ ПОМИЛИВСЯ БРАТАН!!!!!!!"
+     when (operation) {
+          "+" -> println(num1 + num2)
+          "-" -> println(num1 - num2)
+          "/" -> println(num1 / num2)
+          "*" -> println(num1 * num2)
+          else -> println("ТИ ПОМИЛИВСЯ БРАТАН!!!!!!!")
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     println("Вас вітає армія України. Плати податки")
+     println("Відбувається злом казино. Ви викачано 17 593 гривні з 3000000000 грівні.Продолжіть?\n")
+}
